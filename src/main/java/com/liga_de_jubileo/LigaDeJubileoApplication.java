111a50f75acce00a3473018f5671d5f2d4008c52
clorenzo <clorenzo@imagar.com>
2024-12-12 12:47:35 +0100

    init commit - 2

---
+++ src/main/java/com/liga_de_jubileo/LigaDeJubileoApplication.java
@@ -0,0 +1,13 @@
+package com.liga_de_jubileo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaDeJubileoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaDeJubileoApplication.class, args);
+	}
+
+}
